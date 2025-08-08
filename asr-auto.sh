e2b60a952776d22c02665a62e74494f3c4774a74
SelloydKO <selloyd.fernandes@kocycle.com>
2025-08-08 14:50:04 +0100

    Update asr-auto.sh

--- asr-auto.sh
+++ asr-auto.sh
@@ -6,7 +6,7 @@ diskutil erasedisk APFS u /dev/disk0
 
 if [ -f "/Volumes/ESR/ES.dmg" ]
 then
-  asr -s "/Volumes/ESR/ES.dmg" -t "/Volumes/u" --noprompt --erase 
+  asr -s "/Volumes/ESR/ES.dmg" -t "/Volumes/u" --noprompt --erase
   reboot
 else
   echo "File /Volumes/ESR/ES.dmg not found, skipping."
