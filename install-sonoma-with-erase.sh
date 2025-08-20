04530cd1456cfe1f7ab69c599e6cab6357b85088
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2025-08-20 09:39:55 +0100

    Create install-sonoma-with-erase.sh

---
+++ install-sonoma-with-erase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Created by Selloyd Fernandes
+
+echo -e "\033c" 
+
+diskutil erasedisk APFS untitled /dev/disk0
+
+if [ -f "/Volumes/ESR/Install macOS Sonoma.dmg" ]
+then
+  /Volumes/Install\ macOS\ Sonoma/Install\ macOS\ Sonoma.app/Contents/macOS/InstallAssistant 
+else
+  echo "Image /Volumes/ESR/Install macOS Sonoma/ not found."
+fi
+
+exit 0
