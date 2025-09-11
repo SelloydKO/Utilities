a57664228b5da88ed0214d8997cb89e2cd55fbbf
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2025-09-11 15:08:02 +0100

    Update asr-auto.sh

--- asr-auto.sh
+++ asr-auto.sh
@@ -6,15 +6,15 @@ echo -e "\033c"
 diskutil erasedisk APFS u /dev/disk0
 
 if [ -d "/Volumes/u" ]
-then
- echo "Onwards to ASR"
- if [ -f "/Volumes/ESR/ES.dmg" ]
  then
-   asr -s "/Volumes/ESR/ES.dmg" -t "/Volumes/u" --noprompt --erase
-   reboot
- else
-   echo "File /Volumes/ESR/ES.dmg not found, skipping."
- fi
+  echo "Onwards to ASR"
+  if [ -f "/Volumes/ESR/ES.dmg" ]
+  then
+    asr -s "/Volumes/ESR/ES.dmg" -t "/Volumes/u" --noprompt --erase
+    reboot
+  else
+    echo "File /Volumes/ESR/ES.dmg not found, skipping."
+  fi
 else
   echo "Failed to wipe primary disk"
 fi
