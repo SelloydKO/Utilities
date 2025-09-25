a552c6fdcd76c9ad026a2744be8b60447e5ee59c
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2025-09-25 10:24:09 +0100

    Update install-sonoma-with-erase.sh

--- install-sonoma-with-erase.sh
+++ install-sonoma-with-erase.sh
@@ -5,7 +5,7 @@ echo -e "\033c"
 
 diskutil erasedisk APFS untitled /dev/disk0
 
-if [ -f "/Volumes/ESR/Install macOS Sonoma.dmg" ]
+if [ -f "/Volumes/Install macOS Sonoma.dmg" ]
 then
   /Volumes/Install\ macOS\ Sonoma/Install\ macOS\ Sonoma.app/Contents/macOS/InstallAssistant 
 else
