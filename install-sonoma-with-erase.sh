9063c4cc4c4fb9d8120171e6691acb401c6ae3c8
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2025-10-08 09:46:08 +0100

    Update install-sonoma-with-erase.sh

--- install-sonoma-with-erase.sh
+++ install-sonoma-with-erase.sh
@@ -5,12 +5,12 @@ echo -e "\033c"
 
 diskutil erasedisk APFS untitled /dev/disk0
 
-if [ -e "/Volumes/Image\ Volume/" ]
-then
+{
   /Volumes/Image\ Volume/Install\ macOS\ Sonoma.app/Contents/macOS/InstallAssistant 
-else
+} ||
+{
   echo "Not booted into Install macOS Sonoma. Will attempt to install via external"
   /Volumes/Install\ macOS\ Sonoma/Install\ macOS\ Sonoma.app/Contents/macOS/InstallAssistant 
-fi
+}
 
 exit 0
