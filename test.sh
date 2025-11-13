9653ac792f6391f2d0ddd6d54e5279ddbe3cbf99
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2025-11-13 10:13:05 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -26,10 +26,23 @@ tput reset
     echo -e "\033[1mKeyboard Info:\033[0m";
     ioreg -l | grep "KeyboardLanguage";
 
-     # T2 info
+
+    cpu_brand=$(sysctl -n machdep.cpu.brand_string 2>/dev/null)
+    if [[ "$cpu_brand" =~ ^Apple\ M ]]; then
+    echo "This is an M-series MacBook."
+    elif [[ "$cpu_brand" =~ Intel ]]; then
+    hardware_info=$(system_profiler SPHardwareDataType 2>/dev/null)
+    if echo "$hardware_info" | grep -q "iBridge"; then
+    {
+    # T2 info
+    echo "This is a T2 MacBook."
     echo -e "\033[1mT2 Info:\033[0m";
     sysctl -n machdep.cpu.brand_string;
     ioreg -l | grep -e "AppleRawCurrentCapacity" -e "AppleRawMaxCapacity"
+    }
+    else
+    echo "This is an Intel MacBook without T2 chip."
+    fi
 
 } | awk -F '[:=]' '{
     gsub(/^ +| +$/, "   ", $1);
