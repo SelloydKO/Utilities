b60a26a555e6a17c40fe985ea958e59b0cc4ac1f
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2026-02-09 09:57:47 +0000

    Update apple-spec.sh
    Signed-off-by: Selloyd - KOcycle <selloyd.fernandes@kocycle.com>

--- apple-spec.sh
+++ apple-spec.sh
@@ -1,8 +1,38 @@
 #!/bin/bash 
 # KOcycle Helper Script - get info for macs to fill out in Razor
+# Created by Selloyd
 
 # Reset terminal to keep things clean
 tput reset
+
+#variables
+inner_width=58
+total_width=$((inner_width + 2))
+title="Apple full spec report"
+creator="- by Selloyd Fernandes"
+
+# Top border
+printf '%*s\n' "$total_width" '' | tr ' ' '*'
+
+# Empty line (top padding)
+printf '*%*s*\n' "$inner_width" ''
+
+# Main title (perfectly centered)
+title_len=${#title}
+left_title=$(((inner_width - title_len) / 2))
+printf '*%*s%s%*s*\n' "$left_title" '' "$title" "$left_title" ''
+
+# Creator/subtitle (perfectly centered)
+creator_len=${#creator}
+left_creator=$(((inner_width - creator_len) / 2))
+printf '*%*s%s%*s*\n' "$left_creator" '' "$creator" "$left_creator" ''
+
+# Empty line (bottom padding)
+printf '*%*s*\n' "$inner_width" ''
+
+# Bottom border
+printf '%*s\n' "$total_width" '' | tr ' ' '*'
+
 
 # Collect and format system information
 {
@@ -29,16 +59,13 @@ tput reset
 
     cpu_brand=$(sysctl -n machdep.cpu.brand_string 2>/dev/null)
     if [[ "$cpu_brand" =~ ^Apple\ M ]]; then
-    {
-        # M-series Info
-        echo "This is an M-series MacBook";
-    }
+    echo "This is an M-series MacBook.";
     elif [[ "$cpu_brand" =~ Intel ]]; then
     hardware_info=$(system_profiler SPHardwareDataType 2>/dev/null)
     if echo "$hardware_info" | grep -q "iBridge"; then
     {
         # T2 info
-        echo "This is a T2 MacBook"
+        echo "This is a T2 MacBook."
         echo -e "\033[1mT2 Info:\033[0m";
         sysctl -n machdep.cpu.brand_string;
         ioreg -l | grep -e "AppleRawCurrentCapacity" -e "AppleRawMaxCapacity"
@@ -50,17 +77,10 @@ tput reset
     echo "Unknown processor type."
     fi
 
-    # Battery Charge
-    system_profiler SPPowerDataType | grep -e "State";
-
 } | awk -F '[:=]' '{
     gsub(/^ +| +$/, "   ", $1);
     gsub(/^ +| +$/, "   ", $2);
     print $1 ":" $2;
 }'
-
-
-echo -e "Created by \033[1mSelloyd\033[0m"
-
 
 exit 0
