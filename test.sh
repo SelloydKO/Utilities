965817061dc7c48a445ec328a0d9c082ef9c7701
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2026-02-09 09:54:43 +0000

    Update test.sh
    Signed-off-by: Selloyd - KOcycle <selloyd.fernandes@kocycle.com>

--- test.sh
+++ test.sh
@@ -7,6 +7,8 @@ tput reset
 #variables
 inner_width=58
 total_width=$((inner_width + 2))
+title="Apple full spec report"
+creator="- created by Selloyd Fernandes"
 
 # Top border
 printf '%*s\n' "$total_width" '' | tr ' ' '*'
@@ -15,13 +17,11 @@ printf '%*s\n' "$total_width" '' | tr ' ' '*'
 printf '*%*s*\n' "$inner_width" ''
 
 # Main title (perfectly centered)
-title="Apple Software Restore"
 title_len=${#title}
 left_title=$(((inner_width - title_len) / 2))
 printf '*%*s%s%*s*\n' "$left_title" '' "$title" "$left_title" ''
 
 # Creator/subtitle (perfectly centered)
-creator="- created by Selloyd Fernandes"
 creator_len=${#creator}
 left_creator=$(((inner_width - creator_len) / 2))
 printf '*%*s%s%*s*\n' "$left_creator" '' "$creator" "$left_creator" ''
