543be4a547a1b5ad1a170f118974163155cc3259
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2026-02-09 10:01:11 +0000

    Update apple-spec.sh
    Signed-off-by: Selloyd - KOcycle <selloyd.fernandes@kocycle.com>

--- apple-spec.sh
+++ apple-spec.sh
@@ -59,22 +59,28 @@ printf '%*s\n' "$total_width" '' | tr ' ' '*'
 
     cpu_brand=$(sysctl -n machdep.cpu.brand_string 2>/dev/null)
     if [[ "$cpu_brand" =~ ^Apple\ M ]]; then
-    echo "This is an M-series MacBook.";
+    {
+        # M series Info
+        echo "This is an M-series MacBook";
+        
+    }
     elif [[ "$cpu_brand" =~ Intel ]]; then
     hardware_info=$(system_profiler SPHardwareDataType 2>/dev/null)
     if echo "$hardware_info" | grep -q "iBridge"; then
     {
         # T2 info
-        echo "This is a T2 MacBook."
+        echo "This is a Intel MacBook with T2 chip"
         echo -e "\033[1mT2 Info:\033[0m";
         sysctl -n machdep.cpu.brand_string;
         ioreg -l | grep -e "AppleRawCurrentCapacity" -e "AppleRawMaxCapacity"
     }
     else
-    echo "This is an Intel MacBook without T2 chip."
+    {
+        echo "This is an Intel MacBook without T2 chip"
+    }
     fi
     else
-    echo "Unknown processor type."
+        echo "Unknown processor type"
     fi
 
 } | awk -F '[:=]' '{
