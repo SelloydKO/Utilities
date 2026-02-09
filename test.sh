a00cfb7426d08b57f030eb5e449cd0e865099d00
Selloyd - KOcycle <selloyd.fernandes@kocycle.com>
2026-02-09 09:52:02 +0000

    Update test.sh
    Signed-off-by: Selloyd - KOcycle <selloyd.fernandes@kocycle.com>

--- test.sh
+++ test.sh
@@ -1,6 +1,34 @@
 #!/bin/bash 
 # KOcycle Helper Script - get info for macs to fill out in Razor
 
+inner_width=58
+total_width=$((inner_width + 2))
+
+# Top border
+printf '%*s\n' "$total_width" '' | tr ' ' '*'
+
+# Empty line (top padding)
+printf '*%*s*\n' "$inner_width" ''
+
+# Main title (perfectly centered)
+title="Apple Software Restore"
+title_len=${#title}
+left_title=$(((inner_width - title_len) / 2))
+printf '*%*s%s%*s*\n' "$left_title" '' "$title" "$left_title" ''
+
+# Creator/subtitle (perfectly centered)
+creator="- created by Selloyd Fernandes"
+creator_len=${#creator}
+left_creator=$(((inner_width - creator_len) / 2))
+printf '*%*s%s%*s*\n' "$left_creator" '' "$creator" "$left_creator" ''
+
+# Empty line (bottom padding)
+printf '*%*s*\n' "$inner_width" ''
+
+# Bottom border
+printf '%*s\n' "$total_width" '' | tr ' ' '*'
+
+
 # Reset terminal to keep things clean
 tput reset
 
